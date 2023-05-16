4d424683946c0b4a27d3120a130e1391acdbe55c
fwtiadhs <57071566+fwtiadhs@users.noreply.github.com>
2023-05-16 17:55:42 +0300

    Add project files.

---
+++ ExamThesis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamThesis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
